792398b2911b7564910754bb7bcdd29aeec8d4b0
myUsername <myemail@gmail.comgit config --global user.email myemail@gmail.com>
2023-02-16 17:15:16 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main actual function
  *
- * Return: 0 when successfull
+ * Return: 0 (success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return(0);
 }
